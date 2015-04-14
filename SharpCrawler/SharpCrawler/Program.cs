f3499a3b95dff089219be9d38de54dddcc8cf5dc
Dmitriev Dmitry <ddstmn@gmail.com>
2015-04-14 13:15:43 +0500

    Сделал класс получения web-страницы в строку с парой тестов

--- SharpCrawler/SharpCrawler/Program.cs
+++ SharpCrawler/SharpCrawler/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharpCrawler
 {
@@ -10,6 +6,22 @@ namespace SharpCrawler
     {
         static void Main(string[] args)
         {
+            var crawler = new UrlCrawler("http://www.lenta.ru");
+            try
+            {
+                Console.WriteLine(crawler.GetHttp());
+                Console.ReadLine();
+                GC.Collect();
+                crawler = new UrlCrawler("http://www.lenta122.ru");
+                Console.WriteLine(crawler.GetHttp());
+                Console.ReadLine();
+                crawler = null;
+            }
+            catch (CrawlerException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
