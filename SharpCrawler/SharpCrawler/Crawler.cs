5879ba0d4060ddbaa27475d6a4058bcddefd21e7
Dmitriev Dmitry <ddstmn@gmail.com>
2015-04-19 09:59:16 +0500

    добавили ignore бинарников

---
+++ SharpCrawler/SharpCrawler/Crawler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace SharpCrawler
+{
+    class Crawler
+    {
+        private readonly Downloader _downloader;
+
+        public Crawler(Downloader downloader)
+        {
+            _downloader = downloader;
+        }
+
+        public List<string> GetLinks(string url)
+        {
+            var document = new HtmlDocument();
+
+            var html = _downloader.GetHtml(url).Result;
+            document.LoadHtml(html);
+
+            var uri = new Uri(url);
+            var host = uri.Host.Replace("www.","");
+            var scheme = uri.Scheme;
+
+            var regexAbsoluteLink = new Regex("^http://[[a-z]*[.]*]*" + host + "");
+            var links = document.DocumentNode.SelectNodes("//a[@href]")
+                .Select(link => link.Attributes["href"].Value)
+                .Where(link => regexAbsoluteLink.IsMatch(link))
+                .Distinct().ToList();
+
+            var regexRelativeLink = new Regex("^/");
+            var relativeLinks = document.DocumentNode.SelectNodes("//a[@href]")
+                    .Select(link => link.Attributes["href"].Value)
+                    .Where(link => regexRelativeLink.IsMatch(link))
+                    .Distinct().ToList();
+
+            links.AddRange(relativeLinks.Select(relativeLink => scheme + "://" + host + relativeLink));
+
+            return links;
+        }
+    }
+}
---
+++ SharpCrawler/SharpCrawler/Downloader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SharpCrawler
+{
+    class Downloader
+    {
+        private readonly HttpClient _httpClient;
+
+        public Downloader()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("user-agent",
+                "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+        }
+
+        public async Task<string> GetHtml(string url)
+        {
+            try
+            {
+                var httpResponseMessage = await _httpClient.GetAsync(url);
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                var html = await httpResponseMessage.Content.ReadAsStringAsync();
+                return html;
+            }
+            catch (HttpRequestException httpRequestException)
+            {
+                throw new CrawlerException(
+                    "SharpCrawler.Downloader: Error has occured downloading website! " +
+                    httpRequestException.Message);
+            }
+            catch (Exception exception)
+            {
+                throw new CrawlerException(
+                    "SharpCrawler.Downloader: Error has occured downloading website! " +
+                    exception.Message);
+            }
+        }
+    }
+}
