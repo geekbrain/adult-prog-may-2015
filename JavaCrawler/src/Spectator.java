856475fb1d46d6e9011f2e5a15ef0798280c7efd
wzym <wzym@yandex.ru>
2015-04-20 15:27:39 +0300

    Разделены методы поиска по тегу и обработки атрибута в каждом найденном теге.

--- JavaCrawler/src/Spectator.java
+++ JavaCrawler/src/Spectator.java
@@ -8,27 +8,32 @@ import java.io.IOException;
 public class Spectator {
     private String site;
     private Document document;
+    private Elements elements;
 
     public Spectator() throws IOException {
         site = WebServiceAdapter.getURL();
         setDocument();
-        getAllAttrsByTegAndAttr("a", "href");
+        getAllElementsByTeg("a");
+        handlingHrefs("href");
     }
 
     private void setDocument() {
         try {
-            document = Jsoup.connect(String.valueOf(site)).get();
+            document = Jsoup.connect(site).get();
         } catch (IOException e) {
             System.out.println("Не удаётся подключиться к сайту");
             e.printStackTrace();
         }
     }
 
-    private void getAllAttrsByTegAndAttr(String teg, String attr) {
-        Elements aElements = document.select(teg);
-        for (Element aElement : aElements) {
-            System.out.println(aElement.attr(attr));
-        }
+    private Elements getAllElementsByTeg(String teg) {
+        elements = document.select(teg);
+        return elements;
     }
 
+    private void handlingHrefs(String attr) {
+        for (Element element : elements) {
+            System.out.println(element.attr(attr));
+        }
+    }
 }
