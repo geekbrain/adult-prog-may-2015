1d6b8f8c8f09928de00e4560bdfc87bd43fd133f
wzym <wzym@yandex.ru>
2015-04-20 19:45:45 +0300

    Начал работу над функцией, проверяющей, является ли адрес подлежащим обработке.

--- JavaCrawler/src/Spectator.java
+++ JavaCrawler/src/Spectator.java
@@ -1,9 +1,12 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Spectator {
     private String site;
@@ -33,7 +36,18 @@ public class Spectator {
 
     private void handlingHrefs(String attr) {
         for (Element element : elements) {
-            System.out.println(element.attr(attr));
+            checkLink(element.attr(attr));
         }
+    }
+
+    private void checkLink(String link) {
+        Pattern pat = Pattern.compile("lenta");
+        Matcher mat = pat.matcher(link);
+        Boolean found;
+
+        System.out.println(link);
+        found = mat.find();
+
+        System.out.println(found);
     }
 }
