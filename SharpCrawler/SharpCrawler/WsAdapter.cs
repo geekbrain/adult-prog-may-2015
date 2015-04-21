96299242db547ddb0bd9548db564569b363c8896
Dmitriev Dmitry <ddstmn@gmail.com>
2015-04-21 19:50:07 +0500

    Веб-сервис возвращает тестовые данные. Краулер получает данные с веб-сервиса.

--- SharpCrawler/SharpCrawler/WsAdapter.cs
+++ SharpCrawler/SharpCrawler/WsAdapter.cs
@@ -1,45 +1,35 @@
 ﻿using System.Collections.Generic;
+using SharpCrawler.WsSoap;
 
 namespace SharpCrawler
 {
     class WsAdapter
     {
-        private int _counter = 0;
-        private List<string> _links = new List<string>();
-        private Dictionary<string, int> _namesAmountDictionary =
-            new Dictionary<string, int>();
+        private readonly ServiceClient _wsSoapClient;
 
-        public WsAdapter() { }
+        public WsAdapter()
+        {
+            _wsSoapClient = new ServiceClient();
+        }
 
         public string GetLink()
         {
-            if (_counter > 0)
-            {
-                return null;
-            }
-            _counter++;
-            return "http://lenta.ru/lib/14160711/";
+            return _wsSoapClient.GetLink();
         }
 
         public Dictionary<string, List<string>> GetNamesDictionary()
         {
-            var aliasesDictionary = new Dictionary<string, List<string>>
-            {
-                {"Путин", new List<string> {"Владимир Владимирович", "Президент"}},
-                {"Медведев", null}
-            };
-
-            return aliasesDictionary;
+            return _wsSoapClient.GetNamesDictionary();
         }
 
         public void SendLinks(List<string> links)
         {
-            _links = links;
+            _wsSoapClient.SendLinks(links);
         }
 
         public void SendAmountDictionary(Dictionary<string, int> namesAmountDictionary)
         {
-            _namesAmountDictionary = namesAmountDictionary;
+            _wsSoapClient.SendAmountDictionary(namesAmountDictionary);
         }
     }
 }
--- WsSoap/WsSoap/IService.cs
+++ WsSoap/WsSoap/IService.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 
 namespace WsSoap
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
     [ServiceContract]
     public interface IService
     {
 
         [OperationContract]
-        string GetLink(string link);
+        string GetLink();
 
         [OperationContract]
         Dictionary<string, List<string>> GetNamesDictionary();
@@ -24,33 +18,5 @@ namespace WsSoap
 
         [OperationContract]
         void SendAmountDictionary(Dictionary<string, int> namesAmountDictionary);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
