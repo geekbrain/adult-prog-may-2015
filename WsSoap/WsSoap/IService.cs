547767f997b0edb656e79dfe4d73fdc5bc143432
Dmitriev Dmitry <ddstmn@gmail.com>
2015-04-21 15:40:47 +0500

    Создал ветку и проект веб-сервиса WCF Apllication

---
+++ WsSoap/WsSoap/IService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WsSoap
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+
+        [OperationContract]
+        string GetLink(string link);
+
+        [OperationContract]
+        Dictionary<string, List<string>> GetNamesDictionary();
+
+        [OperationContract]
+        void SendLinks(List<string> links);
+
+        [OperationContract]
+        void SendAmountDictionary(Dictionary<string, int> namesAmountDictionary);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WsSoap/WsSoap/Service1.svc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WsSoap
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        private int _counter = 0;
+        private List<string> _links = new List<string>();
+        private Dictionary<string, int> _namesAmountDictionary =
+            new Dictionary<string, int>();
+
+        public string GetLink(string link)
+        {
+            if (_counter > 0)
+            {
+                return null;
+            }
+            _counter++;
+            return "http://lenta.ru/lib/14160711/";
+        }
+
+        public Dictionary<string, List<string>> GetNamesDictionary()
+        {
+            var aliasesDictionary = new Dictionary<string, List<string>>
+            {
+                {"Путин", new List<string> {"Владимир Владимирович", "Президент"}},
+                {"Медведев", null}
+            };
+
+            return aliasesDictionary;
+        }
+
+        public void SendLinks(List<string> links)
+        {
+            _links = links;
+        }
+
+        public void SendAmountDictionary(Dictionary<string, int> namesAmountDictionary)
+        {
+            _namesAmountDictionary = namesAmountDictionary;
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
