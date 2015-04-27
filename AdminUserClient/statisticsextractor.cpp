1a7514a67a58bb7c2946d81733c301412bd781ae
Mikhail Visloguzov <keytooker@gmail.com>
2015-04-27 13:32:47 +0300

    Включена возможность работы с gsoap из класса statisticextrector.

--- AdminUserClient/statisticsextractor.cpp
+++ AdminUserClient/statisticsextractor.cpp
@@ -2,11 +2,23 @@
 #include "statistics.h"
 #include "namedao.h"
 #include "worksites.h"
+#include "gSoap/soapBasicHttpBinding_USCOREIServiceProxy.h"
+#include "gSoap/BasicHttpBinding_USCOREIService.nsmap"
 #include <QUrl>
+#include <QDebug>
 
 StatisticsExtractor::StatisticsExtractor(QObject *parent) :
     QObject(parent)
 {
+    _ns1__GetNames ns1__GetNames;
+    _ns1__GetNamesResponse ns1__GetNamesResponse;
+    BasicHttpBinding_USCOREIServiceProxy server("http://adultprog2015.somee.com/WsSoap.svc");
+    if (server.GetNames(&ns1__GetNames, ns1__GetNamesResponse) == SOAP_OK) {
+        qDebug() << "True";
+    }
+    else
+        qDebug() << "FAIL";
+
 }
 
 void StatisticsExtractor::getGeneralStatistics(QSharedPointer<GeneralStatistics>& statistics) const
